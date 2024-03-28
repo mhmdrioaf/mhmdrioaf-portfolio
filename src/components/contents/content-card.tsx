ba64ebac044bdba1608c0a6fc8b5c69dbdb5c973
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-03-28 14:31:24 +0700

    chore: changing background color opacity

--- src/components/contents/content-card.tsx
+++ src/components/contents/content-card.tsx
@@ -11,7 +11,7 @@ export default function ContentCard({ content }: IContentCardProps) {
   return (
     <Link
       href={content.slug}
-      className="w-full flex flex-col gap-2 overflow-hidden rounded-md bg-primary/75 hover:bg-primary/100 transition-colors backdrop-blur-sm p-4 mx-auto"
+      className="w-full flex flex-col gap-2 overflow-hidden rounded-md bg-primary/45 hover:bg-primary/100 transition-colors p-4 mx-auto"
     >
       <h3 className="text-2xl font-bold">{content.title}</h3>
       <p className="truncate text-sm">{content.description}</p>
