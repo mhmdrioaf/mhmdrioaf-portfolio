7b068d755b8e440d7a1cd2aa23c80b21f243e935
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-03-30 01:35:38 +0700

    chore: updating header components

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,12 @@
 "use client";
 
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function Header() {
   const [isScrolling, setIsScrolling] = useState(false);
 
-  const headerStyle = `w-full inline-flex left-0 rounded-md px-8 py-8 sticky text-on-primary justify-between transition-all duration-300 z-50 ${
+  const headerStyle = `w-[calc(100%-6rem)] inline-flex left-0 rounded-md px-8 py-8 sticky text-on-primary justify-between transition-all duration-300 z-50 ${
     isScrolling
       ? "bg-primary/85 backdrop-blur-sm opacity-100 top-4"
       : "opacity-0 -top-8"
@@ -28,15 +29,11 @@ export default function Header() {
       <h3 className="font-bold">Rio</h3>
 
       <nav className="inline-flex items-center gap-2">
-        <a href="https://github.com/mhmdrioaf" target="_blank">
-          GitHub
-        </a>
-        <a href="https://linkedin.com/in/rioananta" target="_blank">
-          LinkedIn
-        </a>
-        <a href="https://twitter.com/oirioir" target="_blank">
-          Twitter
-        </a>
+        <Link href="/blog">Projects</Link>
+        <Link href="/links">Links</Link>
+        <Link className="block md:hidden" href="/about">
+          About
+        </Link>
       </nav>
     </header>
   );
