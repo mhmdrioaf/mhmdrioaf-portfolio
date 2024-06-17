d35718ff5c9115c24fb594980cdca32b06b5daa9
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-06-17 18:26:56 +0700

    fix: landing page mobile-responsiveness

--- src/components/about/about-section.tsx
+++ src/components/about/about-section.tsx
@@ -8,40 +8,53 @@ export default function AboutSection() {
       className="w-full min-h-svh p-container-base md:p-container-md gap-4 flex flex-col justify-center items-center bg-background text-on-primary"
     >
       <AboutCard title="About Me">
-        <p>{SELF_DESCRIPTION}</p>
+        <p className="text-sm md:text-base">{SELF_DESCRIPTION}</p>
       </AboutCard>
       <AboutCard title="Education">
-        <b className="text-2xl">{EDUCATIONS.UNIVERSITY.title}</b>
-        <p>{EDUCATIONS.UNIVERSITY.period}</p>
-        <p>{EDUCATIONS.UNIVERSITY.description}</p>
-        <p>{EDUCATIONS.UNIVERSITY.GPA}</p>
+        <b className="text-sm md:text-2xl">{EDUCATIONS.UNIVERSITY.title}</b>
+        <p className="text-sm md:text-base">{EDUCATIONS.UNIVERSITY.period}</p>
+        <p className="text-sm md:text-base">
+          {EDUCATIONS.UNIVERSITY.description}
+        </p>
+        <p className="text-sm md:text-base">{EDUCATIONS.UNIVERSITY.GPA}</p>
       </AboutCard>
       <AboutCard title="Experiences">
         <section id="presentologics" className="w-full flex flex-col gap-1">
-          <b className="text-2xl">{EXPERIENCES.PRESENTOLOGICS.title}</b>
-          <p>{EXPERIENCES.PRESENTOLOGICS.period}</p>
-          <p className="whitespace-break-spaces">
+          <b className="text-sm md:text-2xl">
+            {EXPERIENCES.PRESENTOLOGICS.title}
+          </b>
+          <p className="text-sm md:text-base">
+            {EXPERIENCES.PRESENTOLOGICS.period}
+          </p>
+          <p className="text-sm md:text-base whitespace-break-spaces">
             {EXPERIENCES.PRESENTOLOGICS.description}
           </p>
         </section>
-        <section id="district" className="w-full flex flex-col gap-1">
-          <b className="text-2xl">{EXPERIENCES.DISTRICT.title}</b>
-          <p>{EXPERIENCES.DISTRICT.period}</p>
-          <p className="whitespace-break-spaces">
+        <section id="district" className="mt-2 w-full flex flex-col gap-1">
+          <b className="text-sm md:text-2xl">{EXPERIENCES.DISTRICT.title}</b>
+          <p className="text-sm md:text-base">{EXPERIENCES.DISTRICT.period}</p>
+          <p className="text-sm md:text-base whitespace-break-spaces">
             {EXPERIENCES.DISTRICT.description}
           </p>
         </section>
-        <section id="smk_marketplace" className="w-full flex flex-col gap-1">
-          <b className="text-2xl">{EXPERIENCES.SMK_MARKETPLACE.title}</b>
-          <p>{EXPERIENCES.SMK_MARKETPLACE.period}</p>
-          <p className="whitespace-break-spaces">
+        <section
+          id="smk_marketplace"
+          className="mt-2 w-full flex flex-col gap-1"
+        >
+          <b className="text-sm md:text-2xl">
+            {EXPERIENCES.SMK_MARKETPLACE.title}
+          </b>
+          <p className="text-sm md:text-base">
+            {EXPERIENCES.SMK_MARKETPLACE.period}
+          </p>
+          <p className="text-sm md:text-base whitespace-break-spaces">
             {EXPERIENCES.SMK_MARKETPLACE.description}
           </p>
         </section>
-        <section id="smk_ppdb" className="w-full flex flex-col gap-1">
-          <b className="text-2xl">{EXPERIENCES.SMK_PPDB.title}</b>
-          <p>{EXPERIENCES.SMK_PPDB.period}</p>
-          <p className="whitespace-break-spaces">
+        <section id="smk_ppdb" className="mt-2 w-full flex flex-col gap-1">
+          <b className="text-sm md:text-2xl">{EXPERIENCES.SMK_PPDB.title}</b>
+          <p className="text-sm md:text-base">{EXPERIENCES.SMK_PPDB.period}</p>
+          <p className="text-sm md:text-base whitespace-break-spaces">
             {EXPERIENCES.SMK_PPDB.description}
           </p>
         </section>
--- src/components/profile/welcome.tsx
+++ src/components/profile/welcome.tsx
@@ -12,20 +12,23 @@ export default function Welcome() {
       id="welcome"
       className="w-full h-svh p-container-base md:p-container-md flex flex-col justify-center items-center bg-background text-foreground"
     >
-      <div className="flex flex-col gap-4 items-center justify-center">
-        <p className="text-base md:text-2xl">Welcome,</p>
+      <div className="flex flex-col gap-2 md:gap-4 items-center justify-center">
+        <p className="text-xs md:text-2xl">Welcome,</p>
         <h3
           className={twMerge(
             angkor.className,
-            "text-xl md:text-3xl font-bold text-primary"
+            "text-sm md:text-3xl font-bold text-primary"
           )}
         >
           {"I'm Muhammad Rio Ananta Firdaus"}
         </h3>
-        <p className="text-base md:text-2xl">
+        <p className="text-xs md:text-2xl">
           {"Fullstack Web (Typescript-Based) Developer"}
         </p>
-        <section id="social-media" className="inline-flex items-center gap-2">
+        <section
+          id="social-media"
+          className="inline-flex mt-2 md:mt-0 items-center gap-2"
+        >
           <Link
             href={SOCIAL_MEDIA.GITHUB}
             className="grid place-items-center p-2 rounded-full border border-primary bg-transparent text-primary hover:bg-primary hover:text-background hover:scale-105 transition-all ease-in-out"
