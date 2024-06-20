b55167054658290d18541324d28751ad9dae5707
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-06-20 12:29:54 +0700

    feat: add intersection observer for the current active section

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,13 @@
 "use client";
 
-import { Angkor } from "next/font/google";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
-import { IoDocuments, IoHome, IoPerson } from "react-icons/io5";
+import { IoArrowBack, IoDocuments, IoHome, IoPerson } from "react-icons/io5";
 import { twMerge } from "tailwind-merge";
 
 export default function Header() {
   const [activeNav, setActiveNav] = React.useState<string>("welcome");
+
   const scrollIntoView = (id: string) => {
     const element = document.getElementById(id);
     setActiveNav(id);
@@ -22,6 +21,26 @@ export default function Header() {
   };
 
   const pathname = usePathname();
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const elements = ["#welcome", "#about", "#projects"];
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setActiveNav(entry.target.id);
+          }
+        });
+      },
+      { threshold: [1.0, 0.1, 1.0] }
+    );
+
+    elements.forEach((element) => {
+      const el = document.querySelector(element);
+      if (el) observer.observe(el);
+    });
+  }, []);
 
   return (
     <header className="fixed right-1/2 bottom-0 md:bottom-1/2 md:right-0 transform translate-y-0 translate-x-1/2 md:translate-x-0 md:translate-y-1/2 z-50 py-container-base md:py-0 px-container-base">
@@ -30,8 +49,9 @@ export default function Header() {
           <>
             <button
               className={twMerge(
-                "rounded-full p-2 grid place-items-center bg-surface text-on-surface hover:bg-on-surface hover:text-surface",
-                activeNav === "welcome" && "bg-on-surface text-surface"
+                "rounded-full p-2 grid place-items-center bg-surface text-on-surface hover:bg-on-surface hover:text-surface hover:scale-[1.2] transition-transform duration-150",
+                activeNav === "welcome" &&
+                  "bg-on-surface text-surface scale-[1.2]"
               )}
               onClick={() => scrollIntoView("welcome")}
             >
@@ -39,8 +59,9 @@ export default function Header() {
             </button>
             <button
               className={twMerge(
-                "rounded-full p-2 grid place-items-center bg-surface text-on-surface hover:bg-on-surface hover:text-surface",
-                activeNav === "about" && "bg-on-surface text-surface"
+                "rounded-full p-2 grid place-items-center bg-surface text-on-surface hover:bg-on-surface hover:text-surface hover:scale-[1.2] transition-transform duration-150",
+                activeNav === "about" &&
+                  "bg-on-surface text-surface scale-[1.2]"
               )}
               onClick={() => scrollIntoView("about")}
             >
@@ -48,8 +69,9 @@ export default function Header() {
             </button>
             <button
               className={twMerge(
-                "rounded-full p-2 grid place-items-center bg-surface text-on-surface hover:bg-on-surface hover:text-surface",
-                activeNav === "projects" && "bg-on-surface text-surface"
+                "rounded-full p-2 grid place-items-center bg-surface text-on-surface hover:bg-on-surface hover:text-surface hover:scale-[1.2] transition-transform duration-150",
+                activeNav === "projects" &&
+                  "bg-on-surface text-surface scale-[1.2]"
               )}
               onClick={() => scrollIntoView("projects")}
             >
@@ -59,29 +81,12 @@ export default function Header() {
         )}
 
         {pathname !== "/" && (
-          <>
-            <Link
-              href="/"
-              className="rounded-full p-2 grid place-items-center bg-surface text-on-surface hover:bg-on-surface hover:text-surface"
-              onClick={() => scrollIntoView("welcome")}
-            >
-              <IoHome className="w-4 h-4" />
-            </Link>
-            <Link
-              href="/#about"
-              className="rounded-full p-2 grid place-items-center bg-surface text-on-surface hover:bg-on-surface hover:text-surface"
-              onClick={() => scrollIntoView("about")}
-            >
-              <IoPerson className="w-4 h-4" />
-            </Link>
-            <Link
-              href="/#projects"
-              className="rounded-full p-2 grid place-items-center bg-surface text-on-surface hover:bg-on-surface hover:text-surface"
-              onClick={() => scrollIntoView("projects")}
-            >
-              <IoDocuments className="w-4 h-4" />
-            </Link>
-          </>
+          <button
+            className="rounded-full p-2 grid place-items-center bg-surface text-on-surface hover:bg-on-surface hover:text-surface hover:scale-[1.2] transition-transform duration-150"
+            onClick={() => router.back()}
+          >
+            <IoArrowBack className="w-4 h-4" />
+          </button>
         )}
       </nav>
     </header>
